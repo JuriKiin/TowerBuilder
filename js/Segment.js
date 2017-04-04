ef7a3b814dec0b7a48650927dbc1f1f605d60099
Nikolas Whiteside <nrw6218@rit.edu>
2017-04-04 17:02:26 -0400

    Segment class was updated with complete method implementation and comments. The main game file was updated with a GAMESTAT machine in an update method to control what is being displayed on the screen at a given time.

--- js/Segment.js
+++ js/Segment.js
@@ -1,19 +1,27 @@
-class Segment
-{
-    constructor(options)
-    {   
+/*
+    Authors: Juri Kiin, Nikolas Whiteside
+    Date: 4/4/2017
+*/
+
+class Segment{
+
+    constructor(options){   
         options = options || {};
 
-        this.src = 'url';
-        this.speed = 1.0;
-        //this.xPos = 0;
-        this.yPos = 0;
-        this.width = 100;
+        //Reference to canvas
+        this.ctx = options.ctx;
+
+        //Segment at the top of the building
+        this.prevSegment = options.prevSegment;
+
+        //Sprite image
+        this.src = options.url;
+        this.speed = options.speed;
+        this.width = this.prevSegment.clipWidth;
         this.height = 50;
-        this.clipX = 0;
-        this.clipY = 0;
-        this.clipWidth = 0;
-        this.clipHeight = 0;
+        this.clipWidth = this.width;
+        this.clipHeight = this.height;
+        this.moving = true;
 
         if(spawnDirction == 0)  //Spawn left side
         {
@@ -21,41 +29,118 @@ class Segment
         }
         else    //Spawn right side
         {
-            this.xPos = window.innerWidth;
+            this.xPos = this.ctx.canvas.clientWidth - this.width;
         }
+        this.yPos = this.ctx.canvas.clientHeight/2;
+        this.clipX = this.xPos;
+        this.clipY = this.yPos;
     }
 
-    /*
-    clip position and width
-    */
+    /* Getters and Setters */
 
     get clipX(){
         return clipX;    
     }
+
     set clipX(value){
         this.clipX = value;
     }
+
     get clipWidth(){
         return clipWidth;
     }
+
     set clipWidth(value){
         this.clipWidth = value;
     }
+
     get xPos(){
         return xPos;
     }
+
     set xPos(value){
         this.xPos = value;
     }
+
     get yPos(){
         return yPos;
     }
+    
     set yPos(value){
         this.yPos = value;
     }
 
+    /* Methods */
 
+    //Checks the edges of this Segment
+    //with the previous segment
+    CheckEdges(){
+        //Return false if this segment and the previous segment do not line up
+        if(this.xPos > prevSegment.clipX() + prevSegment.clipWidth()){
+            return false;
+        }
+        if(prevSegment.clipX() > this.xPos + this.width){
+            return false;
+        }
 
+        //Clip the edges if there is any hangoff
+        if(this.xPos < prevSegment.clipX()){
+            var difference = prevSegment.clipX() - this.xPos;
+            this.clipX += difference;
+            this.clipWidth -= difference;
+        }
 
+        if(this.xPos + this.width > prevSegment.clipX() + prevSegment.clipWidth()){
+            var difference = (prevSegment.clipX() + prevSegment.clipWidth()) - (this.xPos + this.width);
+            this.clipWidth -= difference;
+        }
+
+        //Otherwise, return true
+        return true;
+    }
+
+    //Moves the segment back
+    //and forth
+    MoveSegment(){
+        //Move at the given speed
+        this.xPos += this.speed;
+
+        //Bounce off the two sides
+        if(this.xPos <= 0){
+            this.xPos = 0;
+            this.speed *= -1;
+        }
+        if(this.xPos + this.width >= ctx.canvas.clientWidth){
+            this.xPos = this.ctx.canvas.clientWidth - this.xPos;
+            this.speed *= -1;
+        }
+
+        //Update the clip position
+        this.clipX = this.xPos;
+    }
+
+    //Shifts the segment down a level
+    //according to its height
+    ShiftDown(){
+        this.yPos -= this.height;
+        this.clipY = this.yPos;
+    }
+
+    //Moves the block and
+    //checks for user input
+    //to add it to the top
+    Update(){
+        if(this.moving){
+            MoveSegment();
+        }else{
+            CheckEdges();
+        }
+    }
+
+    //Draws the segment
+    //on the canvas
+    Draw(){
+        ctx.drawImage(src, this.clipX, this.clipY, this.clipWidth, this.Height, this.xPos, this.yPos, this.width, this.height);
+    }
 
 }
--- js/game.js
+++ js/game.js
@@ -1,27 +1,50 @@
 /*
-    game.js.
-    Written by Juri Kiin and Nikolas Whiteside
-    Created: 4/4/2017
-
-
+    Authors: Juri Kiin, Nikolas Whiteside
+    Date: 4/4/2017
 */
-(function(){
-    "use-strict";
 
-    var playButton = document.getElementById('PlayButton');
+"use-strict";
+
+(function(){
+
+    //Canvas variables
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext("2d");
 
-    var segmentImage;   // = 'media/segment.png';
+    //Game States
+    GAME_STATE = Object.freeze({
+        MENU,
+        GAME,
+        GAMEOVER
+    });
+    var gameState;
 
-    window.onload = function()  //Init function
+    var playButton = document.getElementById('PlayButton');
+
+    //Segment variables
+    var segmentImage1;
+    var segmentImage2;
+    var segmentImage3;
+
+    var oldSegments = [];
+
+    //Initialize function
+    window.onload = function()
     {
+        gameState = GAME_STATE.MENU;
+    }
 
-
-
-    };
-
-
+    //Runs the main game loop
+    function Update(){
+        switch(gameState){
+            case MENU:
+                break;
+            case GAME:
+                break;
+            case GAMEOVER:
+                break;
+        }
+    }
     
 
 
